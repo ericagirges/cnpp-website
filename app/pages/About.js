0376562b236645902138893d2399feec2d0506b9
Erica Girges <erica.girges@zendesk.com>
2022-02-23 20:41:23 -0800

    progress on About template styling tweaks

--- app/pages/About.js
+++ app/pages/About.js
@@ -5,7 +5,7 @@ function About() {
         <div>
       <div className="relative bg-white">
         <div className="lg:absolute lg:inset-0">
-          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2 lg:pr-12">
             <img
               className="h-56 w-full object-cover lg:absolute lg:h-full"
               src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
@@ -16,9 +16,9 @@ function About() {
         <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
           <div className="lg:col-start-2 lg:pl-8">
             <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
-              <h2 className="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Work with us</h2>
+              <h2 className="leading-6 text-sbaBlue font-semibold tracking-wide uppercase">Who we are</h2>
               <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Our Process
+              International Rescue Committee
               </h3>
               <p className="mt-8 text-lg text-gray-500">
                 Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
@@ -63,13 +63,23 @@ function About() {
           </div>
         </div>
       </div>
+      <div className="lg:py-12"></div>
       <div className="relative bg-white">
+      <div className="lg:absolute lg:inset-0">
+          <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:pl-12">
+            <img
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+              alt=""
+            />
+          </div>
+        </div>
         <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
-          <div className="lg:col-start-2 lg:pl-8">
-            <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
-              <h2 className="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Work with us</h2>
+          <div className="lg:col-end-2 lg:pr-8">
+            <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:mr-auto lg:ml-0">
+              <h2 className="leading-6 text-sbaBlue font-semibold tracking-wide uppercase">Program overview</h2>
               <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Our Process
+              SBA Community Navigator Pilot Program
               </h3>
               <p className="mt-8 text-lg text-gray-500">
                 Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
@@ -111,15 +121,6 @@ function About() {
                 </p>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="lg:absolute lg:inset-0">
-          <div className="lg:absolute lg:inset-y-0 lg: right-0 lg:w-1/2">
-            <img
-              className="h-56 w-full object-cover lg:absolute lg:h-full"
-              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
-              alt=""
-            />
           </div>
         </div>
       </div>
