c119513c6849a0543d7598d6da881929d0b709b6
Eric Nelson <enelson@zendesk.com>
2022-02-24 12:14:28 -0500

    Changed logo and header design due to logo colors

--- app/pages/About.js
+++ app/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Header from './components/Header'
 function About() {
     return (
         <div>
+                  <Header/>
+
       <div className="relative bg-white">
         <div className="lg:absolute lg:inset-0">
           <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
@@ -65,7 +67,7 @@ function About() {
       </div>
       <div className="relative bg-white">
         <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
-          <div className="lg:col-start-2 lg:pl-8">
+          <div className=" m-auto">
             <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
               <h2 className="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Work with us</h2>
               <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
--- app/pages/components/Header.js
+++ app/pages/components/Header.js
@@ -1,48 +1,44 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import Image from 'next/image'
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+
 
 export default function Header() {
   return (
-    <Disclosure as="nav" className="bg-sbaBlue">
+    <Disclosure as="nav" className="bg-white">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
+            <div className="flex items-center justify-between h-32">
               <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://i.ibb.co/9q14NqK/SBA-Logo-Horizontal-Reverse-RGB.png"
+                <div className="flex-shrink-0"><a href="/">                  <img
+                    className="block lg:hidden h-16 w-auto"
+                    src="https://i.ibb.co/XZpLBp9/SBA-Powered-By-2color-transparent-FINAL.png"
                     alt="SBA Logo"
                   />
                   <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://i.ibb.co/9q14NqK/SBA-Logo-Horizontal-Reverse-RGB.png"
+                    className="hidden lg:block h-24 w-auto"
+                    src="https://i.ibb.co/XZpLBp9/SBA-Powered-By-2color-transparent-FINAL.png"
                     alt="SBA Logo"
-                  />
+                  /></a>
+
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                    <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300  hover:text-white" */}
+                    <a href="/" className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
                       Home
                     </a>
                     <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      href="/about"
+                      className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
                     >
                       About
                     </a>
                     <a
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      href="/contact"
+                      className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Contact Us
                     </a>
@@ -51,7 +47,7 @@ export default function Header() {
               </div>
               <div className="-mr-2 flex sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -65,34 +61,27 @@ export default function Header() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300  hover:text-white" */}
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                href="/"
+                className="text-gray-500 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
               >
-                Dashboard
+                Home
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                href="/about"
+                className="text-gray-500 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
               >
-                Team
+                About
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                href="/contact"
+                className="text-gray-500 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
               >
-                Projects
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Calendar
+                Contact
               </Disclosure.Button>
             </div>
 
