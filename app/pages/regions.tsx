a9df7f2bdebb6fd10373d4814928e3fe04f5ac2b
Erica Girges <erica.girges@zendesk.com>
2022-03-22 12:28:23 -0700

    progress on news

---
+++ app/pages/regions.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from './components/Header'
+import RegionalLanding from './components/RegionalLandingContent'
+import Footer from './components/Footer'
+const regions: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Community Navigator Pilot program</title>
+        <link rel="icon" href={`/favicon.ico`} />
+      </Head>
+      <div className="h-full">
+        <Header />
+        <RegionalLanding />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default regions;
