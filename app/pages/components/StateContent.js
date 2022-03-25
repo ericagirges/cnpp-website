08507a7c3fc3273cf4e538cc9690dbf71568e121
tipenehughes <tipenehughes@gmail.com>
2022-03-25 17:31:52 -0400

    added head and small style changes to state page

--- app/pages/components/StateContent.js
+++ app/pages/components/StateContent.js
@@ -92,7 +92,7 @@ const StateContent = ({ data, state }) => {
                 </p>
               ) : (
                 <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-                  <div className="px-4 py-5 sm:px-6">
+                  <div className="px-4 py-5 sm:px-6 bg-gray-50">
                     <h3 className="text-lg font-medium leading-6 text-gray-900">
                       {event.title}
                     </h3>
