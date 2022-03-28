27ed9ca32000e22cfee9cae15d693e5521edb4d3
tipenehughes <tipenehughes@gmail.com>
2022-03-28 11:30:19 -0400

    Added layout component

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,23 +1,18 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+
 import HeroHeader from './components/HeroHeader'
 import InfoBox from './components/InfoBox'
 import InfoBlob from './components/InfoBlob'
 import HorizontalCard from './components/HorizontalCard'
-import Footer from './components/Footer'
+
 
 const Landing: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Community Navigator Pilot program</title>
-        <link rel="icon" href={`/favicon.ico`} />
-      </Head>
       <HeroHeader />
       <HorizontalCard />
       <InfoBox />
       <InfoBlob />
-      <Footer />
     </div>
   )
 }
