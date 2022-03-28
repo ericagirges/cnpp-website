e15bbf8659093900bfbd65c750b16e7f1e90842a
tipenehughes <tipenehughes@gmail.com>
2022-03-28 11:30:19 -0400

    Added layout component

---
+++ app/pages/components/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Community Navigator Pilot program</title>
+        <link rel="icon" href={`/favicon.ico`} />
+      </Head>
+      <div className="h-full">
+          <Header />
+          {children}
+          <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Layout
--- app/pages/news.tsx
+++ app/pages/news.tsx
@@ -1,22 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from './components/Header'
 import News from './components/NewsContent'
-import Footer from './components/Footer'
+
 const news: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Community Navigator Pilot program</title>
-        <link rel="icon" href={`/favicon.ico`} />
-      </Head>
-      <div className="h-full">
-        <Header />
-        <News />
-        <Footer />
-      </div>
-    </>
-  )
+  return <News />
 }
 
 export default news
