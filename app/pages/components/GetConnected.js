786af5811f53518b8202dae4e07682206437c137
Erica Girges <erica.girges@zendesk.com>
2022-03-28 09:51:57 -0700

    get connected

--- app/pages/components/GetConnected.js
+++ app/pages/components/GetConnected.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
 
 const GetConnected = () => {
 
   return (
     <div className='fixed bottom-80 right-0 w-60 h-32 border-t-8 border-sbaYellow p-4 shadow-lg'>
         {/* <div className='w-full h-full opacity-40'></div> */}
-        <h6 className='text-lg font-semibold text-center'>Get Connected</h6>
-        <p></p>
+        <h6 className='text-lg font-semibold text-center'>Ready to meet with a Counselor?</h6>
+        <button>Click here to get connected</button>
     </div>
   )
 }
