a365b5441b7ff62c855a8f4f22336088997ae7e1
Erica Girges <erica.girges@zendesk.com>
2022-03-31 13:22:01 -0700

    cropped image for modal compatibility - set max width on inner modal container

--- app/pages/components/news/ModalLayout.js
+++ app/pages/components/news/ModalLayout.js
@@ -15,13 +15,13 @@ const ModalLayout = ({setShowModal, content, activeIdx}) => {
       <div className="flex-shrink-0">
 
       <img
-        className="w-1/3 h-1/3 object-cover rounded-lg mx-auto pt-10"
+        className="w-1/3 h-1/3 object-cover rounded-lg mx-auto mt-10"
         src={content[index].image}
         alt={`${content[index].title} business owners`}
       />
 
       </div>
-      <div className="flex flex-col text-center justify-center pt-4 md:pt-8 px-10">
+      <div className="flex flex-col text-center justify-center pt-4 md:pt-8 px-10 max-w-7xl mx-auto">
         <h3 className="text-lg md:text-3xl font-bold text-sbaBlue drop-shadow-md mb-8">{content[index].title}</h3>
         <div className='object-contain max-h-72 overflow-auto'>
         <p className='bg-white text-black md:p-10 mt-4 text-sm md:text-lg text-left'>{content[index].description}</p>
