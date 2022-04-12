118a8f3b7567aaf0a378bb4693c15b5fa5358c6f
Erica Girges <erica.girges@zendesk.com>
2022-04-12 09:23:01 -0700

    first commit on branch

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,29 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 import Layout from './components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-58440TT5Z7`}
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-58440TT5Z7', { page_path: window.location.pathname });
+            `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
