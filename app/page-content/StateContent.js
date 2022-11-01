f499c20aeddc86d51ff4428070c0669d80c8b98c
marykathoeser <100617237+marykathoeser@users.noreply.github.com>
2022-11-01 10:48:35 -0400

    Update StateContent.js

--- app/page-content/StateContent.js
+++ app/page-content/StateContent.js
@@ -316,7 +316,35 @@ export const StateData = {
     ],
     body: 'IRC CNPP will support the small business communities in Iowa by providing information and tools to help stabilize and grow your business, including individualized counseling, mentoring, and training. Business advisors can also help small business owners apply for small business relief grants and loans and assist small business owners who have already received COVID-19 relief loans prepare documentation in support of loan forgiveness. Partners in this project also have business accelerator programs, lending services, and expertise in scaling your agriculture business. Interpretation services for all services and trainings are available upon request.',
     events: [
-      
+  {
+        title: 'The Story of East Village',
+        date: 'November 4',
+        time: '12-1 PM CT',
+        type: 'In Person',
+        location: 'The Iowa Center (2210 Grand Ave, Des Moines, IA 50312)',
+        link: 'https://theiowacenter.org/event/the-story-of-the-east-village/',
+          },
+              {
+        title: 'Small Business Essential: Year-End Tax Planning For Small Businesses',
+        date: 'November 15',
+        time: '12-1 PM CT',
+        type: 'Online',
+        link: 'https://theiowacenter.org/event/small-business-essential-year-end-tax-planning-for-small-businesses-2/',
+          },
+                  {
+        title: 'Always Ready: Business Plan',
+        date: 'November 16',
+        time: '10-11 AM CT',
+        type: 'Online',
+        link: 'https://theiowacenter.org/event/always-ready-business-plan-21/',
+          },
+                      {
+        title: 'Always Ready: Financials',
+        date: 'November 30',
+        time: '12-1 PM CT',
+        type: 'Online',
+        link: 'https://theiowacenter.org/event/always-ready-financials-21/',
+          },
     ],
     upcoming: {
       text: 'Click here for Iowa Small Business Development Center to see more upcoming events and trainers near you.',
