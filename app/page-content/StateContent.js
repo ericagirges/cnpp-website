b5d5ea3df577730a59bdda087d6a6194dfdcfe75
marykathoeser <100617237+marykathoeser@users.noreply.github.com>
2022-11-30 10:32:45 -0700

    Update StateContent.js

--- app/page-content/StateContent.js
+++ app/page-content/StateContent.js
@@ -132,14 +132,14 @@ export const StateData = {
       },
     ],
     body: 'IRC CNPP will support the small business communities in metro Atlanta and the surrounding areas by providing information and tools to help stabilize and grow local business, including individualized counseling, mentoring, and training. Business advisors can also help small business owners apply for small business relief grants and loans and assist small business owners who have already received COVID-19 relief loans prepare documentation in support of loan forgiveness.  This includes the current City of Atlanta Resurgence Grant.   Partners in this project also have business incubator programs for women and BIPOC entrepreneurs.  Interpretation services for all services and trainings are available upon request.',
-    events: [   
-        {
-        title: 'Grow with Google- Learn the Basics of Google Ads',
+    events: [  
+            {
+        title: 'Learn the Basics of Google Ads',
         date: 'December 8',
-        time: '12-1 PM ET',
+        time: '12-1pm ET',
         type: 'Online',
         link: 'https://register.gotowebinar.com/register/8923514751480661264',
-          },
+      },
     ],
     upcoming: {
       text: 'Click here for Georgia Small Business Development Center-Minority Office to see a list of upcoming events and trainers in the network.',
