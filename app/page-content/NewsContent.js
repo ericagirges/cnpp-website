4f1cf3101c5acc0f11830f8f939dd342d616016d
marykathoeser <100617237+marykathoeser@users.noreply.github.com>
2023-06-28 20:24:43 -0600

    Update NewsContent.js

--- app/page-content/NewsContent.js
+++ app/page-content/NewsContent.js
@@ -6,6 +6,12 @@ export const NewsContent = [
       'Check back soon for updates and get connected to a CNPP counselor.',
     link: ""
   },
+    {
+    index: 1,
+    date: 'June 28, 2023',
+    content: "IRC's Community Navigator Pilot Program celebrated two years since its launch by hosting a press briefing last week, welcoming SBA Associate Administrator Mark Madrid, several local Georga partner organizations, as well as several entrepreneurs who participated in CNPP. Read about the Atlanta Journal Constitution's coverage of the event via the link below.",
+    link: "https://www.ajc.com/news/georgia-news/sba-official-spotlights-immigrant-entrepreneurs-in-atlanta/LYOUZSTCIFDXVLPPMUK67OYVHY/"
+   },
   // This is where you can add additional news updates
   /*
   {
