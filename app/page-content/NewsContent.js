9159c4a198c16967c1538a156bc88b6a71f77b5f
marykathoeser <100617237+marykathoeser@users.noreply.github.com>
2023-06-28 20:31:34 -0600

    Update NewsContent.js

--- app/page-content/NewsContent.js
+++ app/page-content/NewsContent.js
@@ -1,7 +1,7 @@
 // This is to be used for adding to the Latest updates section at the bottom of the News page
 export const NewsContent = [
     {
-    index: 1,
+    index: 0,
     date: 'June 28, 2023',
     content: "IRC's Community Navigator Pilot Program celebrated two years since its launch by hosting a press briefing last week, welcoming SBA Associate Administrator Mark Madrid, several local Georga partner organizations, as well as several entrepreneurs who participated in CNPP. Read about the Atlanta Journal Constitution's coverage of the event via the link below.",
     link: "https://www.ajc.com/news/georgia-news/sba-official-spotlights-immigrant-entrepreneurs-in-atlanta/LYOUZSTCIFDXVLPPMUK67OYVHY/"
